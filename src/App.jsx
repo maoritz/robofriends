ce434b21379ddd5c18a6666f9efb3450ba885497
maoritz <maorattias17@gmail.com>
2020-02-25 01:01:26 +0200

    shorter syntax

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { robots } from "./robots";
+import CardList from "./CardList";
+
+const App = () => {
+  return <CardList robots={robots} />;
+};
+
+export default App;
--- src/CardList.jsx
+++ src/CardList.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  const cardsArray = robots.map((user, i) => {
-    return (
-      <Card
-        key={i}
-        id={robots[i].id}
-        name={robots[i].name}
-        userName={robots[i].username}
-        email={robots[i].email}
-      />
-    );
-  });
-  return <React.Fragment>{cardsArray}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            userName={robots[i].username}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default CardList;
