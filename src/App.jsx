0cbf15ab9e9474a5a206852c41b64f71eabee3ce
maoritz <maorattias17@gmail.com>
2020-02-26 21:00:30 +0200

    Make the page responsive. search bar stay while scrilling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
@@ -37,7 +37,9 @@ class App extends Component {
         <div className="tc">
           <h1>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />;
+          <Scroll>
+            <CardList robots={filteredRobots} />;
+          </Scroll>
         </div>
       );
     }
